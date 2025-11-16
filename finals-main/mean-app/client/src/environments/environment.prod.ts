9ab67836a6fdfb769673ce02aec78b1527735b4f
Gayathri <144080085+Gayathri-08s@users.noreply.github.com>
2025-11-16 18:37:37 +0530

    Delete finals-main/mean-app/client/src/environments directory

--- finals-main/mean-app/client/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: "https://wastezero-wastemanagement-web-zej7.onrender.com"
-};
--- finals-main/mean-app/client/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: "https://wastezero-wastemanagement-web-zej7.onrender.com"
-};
